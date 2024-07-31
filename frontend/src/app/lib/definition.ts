1600b86a4e9554515948b3b7e9345abbf3eda416
Matthieu Scelles <scelles.matthieu@gmail.com>
2024-07-31 15:30:06 +0200

    feat: definitions.ts update

--- frontend/src/app/lib/definition.ts
+++ frontend/src/app/lib/definition.ts
@@ -1,44 +1,38 @@
-export type ProposalCreateData = {
-    oracleAddress: string;
-    title: string;
-    overview: string;
-    externalLink: string;
-    durationTradingPhaseInDays: number;
-    durationAdoptionPhaseInDays: number;
-    adoptionPhaseStartDate: number;
-    adoptionSuccesCriterion: number;
-    adoptionSuccesCriterionDescription: string;
+export type ProposalCreateOnChain = {
+	distributedFileSystemIdentifierCID: string;
 };
 
-export type ProposalGetData = {
-    oracleAddress: string;
-    title: string;
-    overview: string;
-    externalLink: string;
-    durationTradingPhaseInDays: number;
-    durationAdoptionPhaseInDays: number;
-    adoptionPhaseStartDate: number;
-    adoptionSuccessCriterion: number;
-    adoptionSuccessCriterionDescription: string;
-    distributedFileSystemIdentifierCID: string;
-    status: number;
-    balanceYes: bigint;
-    balanceNo: bigint;
-    successCriterionAchieved: boolean;
+export type ProposalGetOnChain = {
+	distributedFileSystemIdentifierCID: string;
+	status: number;
+	balanceYes: bigint;
+	balanceNo: bigint;
+	successCriterionAchieved: boolean;
+};
+
+export type ProposalIPFS = {
+	oracleAddress: string;
+	title: string;
+	overview: string;
+	externalLink: string;
+	tradingPhaseDurationInDays: number;
+	adoptionPhaseDurationInDays: number;
+	adoptionSuccesCriterion: number;
+	adoptionSuccesCriterionDescription: string;
 };
 
 export const ProposalStatus = {
-    WaitingBeforeTradingPhase: 0,
-    TradingPhaseStarted: 1,
-    ProposalRefused: 2,
-    ProposalAccepted: 3,
-    VoteClosed: 4,
+	WaitingBeforeTradingPhase: 0,
+	TradingPhaseStarted: 1,
+	ProposalRefused: 2,
+	ProposalAccepted: 3,
+	VoteClosed: 4,
 };
 
 export const ProposalStatusLabel = [
-    "PENDING",
-    "TRADING",
-    "REFUSED",
-    "ACCEPTED",
-    "CLOSED",
+	"PENDING",
+	"TRADING",
+	"REFUSED",
+	"ACCEPTED",
+	"CLOSED",
 ];
